17b9aedc4b90b7c62ec673782d2e2d68febc92a9
GovindKrishna <govindkrishna1102@gmail.com>
2025-08-07 15:18:39 +0530

    Prepare application for final deployment

--- clinicfrontend/src/services/api.js
+++ clinicfrontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
+const apiURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
 const apiClient = axios.create({
   baseURL: apiURL,
